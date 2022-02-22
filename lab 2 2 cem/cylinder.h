304dc7ffa3f40d5effd25648b317e832af52ad18
Pasha <89980819+Sukhorada1337@users.noreply.github.com>
2022-02-22 12:35:17 +0200

    Update cylinder.h

--- lab 2 2 cem/cylinder.h
+++ lab 2 2 cem/cylinder.h
@@ -1,14 +1,8 @@
-//
-// Created by Пысь on 19.02.2022.
-//
-
-#ifndef UNTITLED1_CYLINDER_H
-#define UNTITLED1_CYLINDER_H
-#include <bits/stdc++.h>
+#ifndef LAB_2_2_CEM_CYLINDER_H
+#define LAB_2_2_CEM_CYLINDER_H
 
 using namespace std;
 class cylinder {
-
 
 private:
     char name[25];
@@ -16,29 +10,30 @@ private:
     float radius;
     float height;
     float volume;
-    static const int maxlen = 255;
-    float setSquare(){
-       square = 2*height*(radius+height);
+    static
+    const int maxlen = 255;
+    float setSquare() {
+        square = 2 * height * 3.15 * (radius + height);
         return square;
     };
-    float setVolume (){
+    float setVolume() {
         volume = square * height;
         return volume;
     };
 
 public:
-    void setName(char *n);
-    void getName(char *n);
-    float getSquare(){
+    void setName(char * n);
+    void getName(char * n);
+    float getSquare() {
         return setSquare();
     };
     void setRadius(float r);
     float getRadius();
     void setHeight(float h);
     float getHeight();
-    float getVolume(){
+    float getVolume() {
         return setVolume();
     };
 };
 
-#endif //UNTITLED1_CYLINDER_H
+#endif //LAB_2_2_CEM_CYLINDER_H
